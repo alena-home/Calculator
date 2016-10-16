c1514c89eb2d6b83ee50a077f94df2581aa06bf9
Alena Karpova <ol-olena@mail.ru>
2016-10-16 18:56:54 +0600

    Add basic operations.

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -8,20 +8,121 @@
 
 #import "ViewController.h"
 
+
+typedef NS_ENUM(NSInteger, Operation) {
+    OperationNone = 0,
+    OperationAdd,
+    OperationSubtract,
+    OperationMultiply,
+    OperationDivide
+};
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    double result;
+    
+    BOOL shouldClear;
+    
+    Operation previousOperation;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    result = 0.0;
+    shouldClear = NO;
+    previousOperation = OperationNone;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)performPreviousOperation {
+    double op1 = result;
+    double op2 = [_inputField.text doubleValue];
+    
+    switch (previousOperation) {
+        case OperationNone:
+            result = op2;
+            break;
+            
+        case OperationAdd:
+            result = op1 + op2;
+            break;
+            
+        case OperationDivide:
+            result = op1 / op2;
+            break;
+            
+        case OperationSubtract:
+            result = op1 - op2;
+            break;
+            
+        case OperationMultiply:
+            result = op1 * op2;
+            break;
+    }
 }
+
+- (void)handleOperation:(Operation)operation {
+    [self performPreviousOperation];
+    
+    previousOperation = operation;
+    shouldClear = YES;
+    _inputField.text = [NSString stringWithFormat:@"%f", result];
+}
+
+#pragma mark - Button Actions
+
+- (IBAction)digitButtonTapped:(id)sender {
+    if (shouldClear) {
+        _inputField.text = @"";
+        shouldClear = NO;
+    }
+    
+    NSString *currentText = _inputField.text;
+    NSInteger digit = ((UIButton *)sender).tag;
+    currentText = [currentText stringByAppendingFormat:@"%ld", (long)digit];
+    _inputField.text = currentText;
+}
+
+- (IBAction)plusButtonTapped:(id)sender {
+    [self handleOperation:OperationAdd];
+}
+
+- (IBAction)minusButtonTapped:(id)sender {
+    [self handleOperation:OperationSubtract];
+}
+
+- (IBAction)multiplyButtonTapped:(id)sender {
+    [self handleOperation:OperationMultiply];
+}
+
+- (IBAction)divideButtonTapped:(id)sender {
+    [self handleOperation:OperationDivide];
+}
+
+- (IBAction)dotButtonTapped:(id)sender {
+    
+}
+
+- (IBAction)clearButtonTapped:(id)sender {
+    previousOperation = OperationNone;
+    _inputField.text = @"";
+    result = 0.0;
+    shouldClear = NO;
+}
+
+- (IBAction)equalButtonTapped:(id)sender {
+    [self performPreviousOperation];
+    
+    previousOperation = OperationNone;
+    shouldClear = YES;
+    _inputField.text = [NSString stringWithFormat:@"%f", result];
+}
+
+
 
 @end
